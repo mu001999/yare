28b43dda342a1a74f4e7d99da997569e3bc0ef7d
mu001999 <mu001999@outlook.com>
2019-05-10 20:24:57 +0800

    add convert for utf8

--- yare.hpp
+++ yare.hpp
@@ -3,16 +3,69 @@
 
 #include <string>
 #include <vector>
+#include <unordered_set>
 
 namespace yare
 {
 namespace details
 {
-    inline std::u32string
-    str_to_utf8(const std::string &str);
+inline std::u32string
+str_to_utf8(const std::string &str)
+{
+    std::u32string result;
+    auto reading = reinterpret_cast<const unsigned char*>(str.c_str());
+	while (*reading)
+	{
+        std::u32string::value_type temp;
+
+		if (*reading < 0b10000000U) result.push_back(*reading++);
+		else if (*reading < 0b11100000U)
+		{
+			(temp = *reading++) <<= 8;
+			result += (temp |= *reading++);
+		}
+		else if (*reading < 0b11110000U)
+		{
+			(temp = *reading++) <<= 8;
+			(temp |= *reading++) <<= 8;
+			result += (temp |= *reading++);
+		}
+		else if (*reading < 0b11111000U)
+		{
+			(temp = *reading++) <<= 8;
+			(temp |= *reading++) <<= 8;
+			(temp |= *reading++) <<= 8;
+			result += (temp |= *reading++);
+		}
+		else break;
+    }
+    return result;
+}
+}
+
+inline std::string
+utf8_to_str(const std::u32string &str)
+{
+    std::string result;
+    for (auto chr : str)
+    {
+        if (chr < 0b10000000U) result += chr;
+        else if (chr < 0b111U << 13) (result += chr >> 8 & 0xFF) += chr & 0xFF;
+        else if (chr < 0b1111U << 20) ((result += chr >> 16 & 0xFF) += chr >> 8 & 0xFF) += chr & 0xFF;
+        else if (chr < 0b11111U << 27) (((result += chr >> 24 & 0xFF) += chr >> 16 & 0xFF) += chr >> 8 & 0xFF) += chr & 0xFF;
+        else break;
+    }
+    return result;
+}
+
+struct NFAState
+{
+    enum class EdgeType {
+        EPSLION, CCL, EMPTY
+    } edgt_type;
+
     
-    inline std::string
-    utf8_to_str(const std::u32string &str);
+};
 } // namespace details
 
 class Pattern
